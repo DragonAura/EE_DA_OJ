cc71c5c358847a54e262d1c8bd16c9ffd6d29b2c
DragonAura <dragonaurapvp@gmail.com>
2023-06-18 13:39:07 +0800

    fix: :bug: fix wrong OJ2 version

--- OJ2.cc
+++ OJ2.cc
@@ -1,71 +1,96 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-int find(int x, int arr[])
+#include <cstdio>
+#include <cstring>
+#define MAX 100000
+using namespace std;
+
+struct node
 {
-    int y = 0;
-    for (y = 0;; y++)
-        if (arr[y] == x)
-            return y;
-}
-int main()
+    node* next;
+    char str[5];
+    int ind;
+} __attribute__((packed));
+
+class Chain
 {
-    int M, N;
-    scanf("%d", &M);
-    scanf("%d", &N);
-    char arr1[100000][4];
-    char arr2[100000][4];
-    int pointm[100000];
-    int pointn[100000];
-    char output[100000][4];
-    int i = 0;
-    for (i = 0; i < M; i++)
+public:
+    Chain(node info[], unsigned long size)
     {
-        scanf("%s", arr1[i]);
-        scanf("%d", &pointm[i]);
-    }
-    int j = 0;
-    for (j = 0; j < N; j++)
-    {
-        scanf("%s", arr2[j]);
-        scanf("%d", &pointn[j]);
-    }
-    int k = 0;
-    int flag = 0;
-    int time = 0;
-    int pm = -1;
-    int pn = -1;
-    int ppm = 0;
-    int ppn = 0;
-    for (k = 0;; k++)
-    {
-        ppm = find(pm, pointm);
-        ppn = find(pn, pointn);
-        flag = strcmp(arr1[ppm], arr2[ppn]);
-        if (flag == 0)
+        inSize = size;
+        unsigned long long aim = (inSize - 1) * inSize / 2 - 1;
+        long long sum = 0;
+        for (int i = 0; i < inSize; i++)
+            sum += info[i].ind;
+        unsigned first_ind = aim - sum;
+        head = &info[first_ind];
+        unsigned newInd = info[first_ind].ind;
+        node* newNode = head;
+        for (int i = 1; i < inSize; i++)
         {
-            strcpy(output[k], arr1[ppm]);
-            pm = ppm;
-            pn = ppn;
-            time++;
+            newNode->next = &info[newInd];
+            newInd = info[newInd].ind;
+            newNode = newNode->next;
         }
-        else
-            break;
     }
-    if (time == 0)
+    friend void Compare(Chain* g1, Chain* g2);
+
+private:
+    long long inSize;
+    node* head = nullptr;
+};
+
+void Compare(Chain* g1, Chain* g2)
+{
+    if (g1->inSize < g2->inSize)  // 强制要求g2更短
+    {
+        Chain* temp = g1;
+        g1 = g2;
+        g2 = temp;
+    }
+    int dif = g1->inSize - g2->inSize;
+    int LastNode = 0;
+    node* n1 = g1->head;
+    node* n2 = g2->head;
+    for (int i = 0; i < dif; i++)
+    {
+        n1 = n1->next;
+    }
+    for (int i = 0; i < g2->inSize; i++)
+    {
+        int res = strcmp(n1->str, n2->str);
+        if (res != 0)
+            LastNode = i + 1;
+        n1 = n1->next;
+        n2 = n2->next;
+    }
+    if (LastNode == g2->inSize)
+    {
         printf("-1");
+    }
     else
     {
-        if (M < N)
-            printf("%d\n", M - time);
-        else
-            printf("%d\n", N - time);
-        int s = time - 1;
-        for (s = time - 1; s >= 0; s--)
+        printf("%d\n", LastNode);
+        node* op = g2->head;
+        for (int i = 0; i < LastNode; i++)
+            op = op->next;
+        while (op != nullptr)
         {
-            printf("%s ", output[s]);
+            printf("%s ", op->str);
+            op = op->next;
         }
     }
-    return 0;
+}
+
+int main()
+{
+    node node1[MAX];
+    node node2[MAX];
+    unsigned M, N;
+    scanf("%u%u", &M, &N);
+    for (int i = 0; i < M; i++)
+        scanf("%s%d", node1[i].str, &node1[i].ind);
+    Chain gM(node1, M);
+    for (int i = 0; i < N; i++)
+        scanf("%s%d", node2[i].str, &node2[i].ind);
+    Chain gN(node2, N);
+    Compare(&gM, &gN);
 }
